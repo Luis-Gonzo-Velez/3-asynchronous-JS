efdfc3e7172c91b89c915a2ece8ad169a3e08dda
Luis-Gonzo-Velez <gadiel2311@outlook.com>
2026-05-07 10:24:04 -0400

    Chapter 43  buildong promises

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("I could not find that file☹️");
+      resolve(data);
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`).then((data) => {
   console.log(`Breed: ${data}`);
 
   superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random `)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then((res) => {
       console.log(res.body.message);
 
       fs.writeFile("dog-img.txt", res.body.message, (err) => {
         if (err) return console.log(err.message);
         console.log("Rondom dog image save to file!");
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
