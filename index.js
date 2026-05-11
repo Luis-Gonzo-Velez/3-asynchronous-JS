832830d4643592d5d36fc9f1d25ec3f3d70fb185
Luis-Gonzo-Velez <gadiel2311@outlook.com>
2026-05-11 09:48:22 -0400

    fixed the bug and got all 3 imgs

--- index.js
+++ index.js
@@ -14,26 +14,33 @@ const writeFilePro = (file, data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(file, data, (err) => {
       if (err) reject("Could not write file 🙂");
-      resolve("succes");
+      resolve("success");
     });
   });
 };
 
 const getDogPic = async () => {
   try {
-    const data = await readFilePro(`${__dirname}/dog.txt`);
+    const data = (await readFilePro(`${__dirname}/dog.txt`)).toString().trim();
     console.log(`Breed: ${data}`);
 
-    const res = await superagent.get(
+    const res1Pro = superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`,
     );
-    console.log(res.body.message);
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`,
+    );
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`,
+    );
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const imgs = all.map((el) => el.body.message);
+    console.log(imgs);
 
-    await writeFilePro("dog-img.txt", res.body.message);
-    console.log("Random dog image save to file!");
+    await writeFilePro("dog-img.txt", imgs.join("\n"));
+    console.log("Random dog image saved to file!");
   } catch (err) {
     console.log(err);
-
     throw err;
   }
 };
